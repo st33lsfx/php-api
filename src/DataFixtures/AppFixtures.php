ecb12a73d7d95579f2b4c0d9fb94a0902b33843f
Ondřej Němec <ondrej.nemec@umimeweby.cz>
2023-04-12 14:53:23 +0200

    add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Followers\Followers;
 use App\Entity\User\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -10,6 +11,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class AppFixtures extends Fixture
 {
     private UserPasswordHasherInterface $hasher;
+
     public function __construct(UserPasswordHasherInterface $hasher)
     {
         $this->hasher = $hasher;
@@ -18,23 +20,36 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-        for($i = 1; $i <= 50; $i++) {
+        $users = [];
+
+        for ($i = 1; $i <= 50; $i++) {
             $user = new User();
             $user->setFirstName('user' . $i);
-            $user->setLastName( 'name-' . $i);
+            $user->setLastName('name-' . $i);
             $user->setNick('admin' . $i);
             $user->setPassword($this->hasher->hashPassword($user, 'password'));
             $user->setRoles(['ROLE_USER']);
             $user->setCreateAt(new \DateTimeImmutable('now'));
 
+            $manager->persist($user);
             $users[] = $user;
         }
 
         foreach ($users as $user) {
-            $manager->persist($user);
+            $followings = array_slice($users, 0, rand(1, 10));
+
+            foreach ($followings as $following) {
+                if ($user !== $following) {
+                    $follower = new Followers();
+                    $follower->setFollow($user);
+                    $follower->setFollower($following);
+                    $follower->setCreateAt(new \DateTimeImmutable('now'));
+
+                    $manager->persist($follower);
+                }
+            }
         }
 
         $manager->flush();
-
     }
 }
